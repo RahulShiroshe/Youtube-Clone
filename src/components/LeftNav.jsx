99e4271c8614d8776c5e988c340aef560f70bf44
RahulShiroshe <121466932+RahulShiroshe@users.noreply.github.com>
2023-03-01 22:22:59 +0530

    Update LeftNav.jsx

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -26,8 +26,8 @@ const LeftNav = () => {
 
     return (
         <div
-            className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 md:z-50 translate-x-[-240px] md:translate-x-0 transition-all ${
-                mobileMenu ? "translate-x-0" : ""
+            className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all ${
+                mobileMenu ?  "translate-x-0" : ""
             }`}
         >
             <div className="flex px-5 flex-col">
@@ -54,7 +54,7 @@ const LeftNav = () => {
                     );
                 })}
                 <hr className="my-5 border-white/[0.2]" />
-                <div className="text-white/[0.5] text-[13px]">
+                <div className="text-white/[0.5] text-[12px]">
                     Clone by: Rahul Shiroshe
                 </div>
             </div>
