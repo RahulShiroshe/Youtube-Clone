14b0527a610a2bb4e6a334e15558216fc2389248
Rahul <rahulpshiroshe@gmail.com>
2023-03-01 14:07:10 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App1 from './App1';
+import App from './App';
+import "./components/index.css"
+// import App3Lifecycle from './App3Lifecycle';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <>
+        <App />
+        {/* <App1 /> */}
+        {/* <App2 /> */}
+        {/* <App3Lifecycle /> */}
+    </>
+);
+
